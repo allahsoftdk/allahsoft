5e869740ec8549797f79ce4792af1e128af44b5c
Frederik Vilhelmsen <frederik@rovi.dk>
2023-02-07 11:14:35 +0100

    prisma testing start

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,10 @@ import { PrismaClient } from '@prisma/client'
 var router = express.Router();
 const prisma = new PrismaClient(); 
 
-
 router.get('/', async (req, res) => {
   const users = await prisma.user.findMany()
   res.render('index', { data: users});   
 })
-
-
-
-router.get('/id/:id', async (req, res) => {
-  const userId = req.params.id;
-  const user = await prisma.user.findUnique({
-    where: {
-      id: Number(userId),
-    },
-  })
-  res.render('index', { data: user}); 
-})
-
-
-
-router.post('/', async (req, res, next) => {
-  const {name,email} = req.body;
-  const user = await prisma.user.create({
-    data: { 
-      email: email,
-      name: name,
-    },
-  });
-  res.json(user);
-});
 
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
-import express from 'express';
-var router = express.Router();
+import express, { json } from 'express'
+import { PrismaClient } from '@prisma/client' 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var router = express.Router();
+const prisma = new PrismaClient();
+
+router.get('/', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users);
+})
+
+router.post('/', async (req, res, next) => {
+  const {name,email} = req.body;
+  const user = await prisma.user.create({
+    data: { 
+      email: email,
+      name: name,
+    },
+  });
+  res.json(user);
 });
+
+router.get('/id/:id', async (req, res) => {
+  const userId = req.params.id;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(userId),
+    },
+  })
+})
 
 export default router
