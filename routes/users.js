1b1e2a0dd98640ade14530099b733b815799a3e8
Victor <victorneerholt@gmail.com>
2023-02-13 09:38:57 +0100

    Start of auth

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 import express, { json } from 'express'
 import prisma from '../prismaClient.js'
+import bcrypt from 'bcrypt'
+
 
 var router = express.Router();
 
@@ -8,15 +10,14 @@ router.get('/', async (req, res) => {
   res.json(users);
 })
 
-router.post('/', async (req, res, next) => {
-  const {name,email} = req.body;
-  const user = await prisma.user.create({
-    data: { 
-      email: email,
-      name: name,
-    },
-  });
-  res.status(200).json(user);
+router.post('/register', async (req, res, next) => {
+  const {name,email, password1, confirmPassword} = req.body;
+  const hashPassword = await bcrypt.hash(password1, 10);
+
+  if(password1 === confirmPassword){
+    res.sendStatus(200)
+  }
+  res.sendStatus(404)
 });
 
 router.get('/:id', async (req, res) => {
